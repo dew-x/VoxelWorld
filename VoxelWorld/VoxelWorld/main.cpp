6c53e113a6530bbb77e66f352317424d93df4d7b
Dewcs <andreualien@hotmail.com>
2015-03-28 12:40:02 +0100

    hg

---
+++ VoxelWorld/VoxelWorld/main.cpp
@@ -0,0 +1,122 @@
+//#define GLEW_STATIC
+#include <GL\glew.h>
+#include <SDL\SDL.h>
+#include <SDL\SDL_opengl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fstream>
+#include <string>
+
+std::string get_file_contents(std::string fname) {
+	std::ifstream file(fname);
+	if (file.fail())	{
+		printf("FAIL Load file %s", fname.c_str());
+	}
+
+	//File contents stores all the text in the file
+	std::string fileContents = "";
+	//line is used to grab each line of the file
+	std::string line;
+	//Get all the lines in the file and add it to the contents
+	while (getline(file, line)) {
+		fileContents += line + "\n";
+	}
+	//Close the file
+	file.close();
+	return fileContents;
+}
+
+// Shader sources
+const GLchar* vertexSource =
+"#version 150 core\n"
+"in vec2 position;"
+"void main() {"
+"   gl_Position = vec4(position, 0.0, 1.0);"
+"}";
+const GLchar* fragmentSource =
+"#version 150 core\n"
+"out vec4 outColor;"
+"void main() {"
+"   outColor = vec4(1.0, 1.0, 1.0, 1.0);"
+"}";
+
+int main(int argc, char *argv[])
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+	SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
+	SDL_GLContext context = SDL_GL_CreateContext(window);	
+	SDL_Event windowEvent;
+	// Initialize GLEW
+	glewExperimental = GL_TRUE;
+	glewInit();
+	// Create Vertex Array Object
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	// Create a Vertex Buffer Object and copy the vertex data to it
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+
+	GLfloat vertices[] = {
+		0.0f, 0.5f,
+		0.5f, -0.5f,
+		-0.5f, -0.5f
+	};
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// shadders
+	// Create and compile the vertex shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+
+	// Create and compile the fragment shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// Link the vertex and fragment shader into a shader program
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glBindFragDataLocation(shaderProgram, 0, "outColor");
+	glLinkProgram(shaderProgram);
+	glUseProgram(shaderProgram);
+
+	// Specify the layout of the vertex data
+	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+	glEnableVertexAttribArray(posAttrib);
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	while (true)
+	{
+		if (SDL_PollEvent(&windowEvent))
+		{
+			if (windowEvent.type == SDL_QUIT) break;
+			if (windowEvent.type == SDL_KEYUP &&
+				windowEvent.key.keysym.sym == SDLK_ESCAPE) break;
+		}
+		// Clear the screen to black
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Draw a triangle from the 3 vertices
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		SDL_GL_SwapWindow(window);
+	}
+	glDeleteProgram(shaderProgram);
+	glDeleteShader(fragmentShader);
+	glDeleteShader(vertexShader);
+
+	glDeleteBuffers(1, &vbo);
+
+	glDeleteVertexArrays(1, &vao);
+	SDL_GL_DeleteContext(context);
+	SDL_Quit();
+	return 0;
+}
