3930b762cbbf73597eb28a3093454237f05a6c3a
mastergun <martimendrugu@gmail.com>
2015-03-29 12:26:47 +0200

    a

---
+++ VoxelWorld/VoxelWorld/Camera.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <GL\glew.h>
+#include <SDL\SDL.h>
+#include <SDL\SDL_opengl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fstream>
+#include <string>
+#include <time.h>
+
+class Camera
+{
+public:
+	Camera();
+	~Camera();
+};
+
