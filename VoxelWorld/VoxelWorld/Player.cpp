df0c9d5ad0f1531a5e59ca6a6126f9e55ff1cb39
dew-x <afigueras@enti.cat>
2015-05-11 11:12:11 +0200

    Fix

--- VoxelWorld/VoxelWorld/Player.cpp
+++ VoxelWorld/VoxelWorld/Player.cpp
@@ -1,11 +1,34 @@
 #include "Player.h"
 
 
-Player::Player()
+Player::Player(glm::vec3 pos)
 {
+	position = pos;
 }
 
 
 Player::~Player()
 {
+}
+
+glm::vec3 Player::getCameraFront(){
+	return{ 0, 0, 0 };
+}
+glm::vec3 Player::getCameraUp(){
+	return{ 0, 0, 0 };
+}
+glm::vec3 Player::getPosition(){
+	return position;
+}
+glm::vec3 Player::getMin(){
+	return{ 0, 0, 0 };
+}
+glm::vec3 Player::getMax(){
+	return{ 0, 0, 0 };
+}
+void Player::addMouseDeltas(float x, float y){
+}
+void Player::moveDeltas(float x, float y){
+}
+void Player::doJump(){
 }
